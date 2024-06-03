96aad6431d1ffadd2f0990b46b50116ebebdf59a
maahi <maahi@gmail.com>
2024-06-03 15:10:08 +0530

    adding java controller file to the repo

---
+++ src/main/java/com/example/love/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.love.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("MyLovE", "I LOVE YOU PALLAVI!");
+        return "index";
+    }
+}
+
